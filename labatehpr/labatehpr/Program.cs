21c7f8247dd28bad6e74d5866bf4f6eb30d408ca
My Name <north_pole@list.ru>
2018-11-15 12:50:55 +0400

    1

--- labatehpr/labatehpr/Program.cs
+++ labatehpr/labatehpr/Program.cs
@@ -16,7 +16,7 @@ namespace labatehpr
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormAircraft());
+            Application.Run(new FormHangar());
         }
     }
 }
