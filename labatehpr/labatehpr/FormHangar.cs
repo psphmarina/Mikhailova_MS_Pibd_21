8948ef4a95014c5b46cd91e16d484a22be2a887c
My Name <north_pole@list.ru>
2018-11-15 13:35:24 +0400

    l4

--- labatehpr/labatehpr/FormHangar.cs
+++ labatehpr/labatehpr/FormHangar.cs
@@ -12,68 +12,100 @@ namespace labatehpr
 {
     public partial class FormHangar : Form
     {
-        Hangar<ITransport> hangar;
+        MultiLevelHangar hangar;
+        private const int countLevel = 5;
         public FormHangar()
         {
             InitializeComponent();
-            hangar = new Hangar<ITransport>(20, pictureBoxHangar.Width, pictureBoxHangar.Height);
-            Draw();
+            hangar = new MultiLevelHangar(20, pictureBoxHangar.Width, pictureBoxHangar.Height);
+            //заполнение listBox
+            for (int i = 0; i < countLevel; i++)
+            {
+                listBoxLevels.Items.Add("Уровень " + (i + 1));
+            }
+            listBoxLevels.SelectedIndex = 0;
         }
         private void Draw()
         {
-            Bitmap bmp = new Bitmap(pictureBoxHangar.Width, pictureBoxHangar.Height);
-            Graphics gr = Graphics.FromImage(bmp);
-            hangar.Draw(gr);
-            pictureBoxHangar.Image = bmp;
+            if (listBoxLevels.SelectedIndex > -1) { 
+                Bitmap bmp = new Bitmap(pictureBoxHangar.Width, pictureBoxHangar.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                hangar[listBoxLevels.SelectedIndex].Draw(gr);
+                pictureBoxHangar.Image = bmp;
+            }
         }
         private void buttonAircraft_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                var car = new Aircraft(100, 1000, dialog.Color);
-                int place = hangar + car;
-                Draw();
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var car = new Aircraft(100, 1000, dialog.Color);
+                    int place = hangar[listBoxLevels.SelectedIndex] + car;
+                    if (place == -1)
+                    {
+                        MessageBox.Show("Нет свободных мест", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    Draw();
+                }
             }
         }
 
         private void buttonFighterAircraft_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                ColorDialog dialogDop = new ColorDialog();
-                if (dialogDop.ShowDialog() == DialogResult.OK)
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    var car = new FighterAircraft(100, 1000, dialog.Color, dialogDop.Color,
-                   true, true, true);
-                    int place = hangar + car;
-                    Draw();
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == DialogResult.OK)
+                    {
+                        var car = new FighterAircraft(100, 1000, dialog.Color, dialogDop.Color,
+                        true, true, true);
+                        int place = hangar[listBoxLevels.SelectedIndex] + car;
+                        if (place == -1)
+                        {
+                            MessageBox.Show("Нет свободных мест", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        Draw();
+                    }
                 }
             }
         }
 
         private void ButtonTake_Click(object sender, EventArgs e)
         {
-            if (maskedTextBox1.Text != "")
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                var car = hangar - Convert.ToInt32(maskedTextBox1.Text);
-                if (car != null)
+                if (maskedTextBox1.Text != "")
                 {
-                    Bitmap bmp = new Bitmap(pictureBoxTake.Width,
+                    var car = hangar[listBoxLevels.SelectedIndex] - Convert.ToInt32(maskedTextBox1.Text);
+                    if (car != null)
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxTake.Width,
                    pictureBoxTake.Height);
-                    Graphics gr = Graphics.FromImage(bmp);
-                    car.SetPosition(5, 15, pictureBoxTake.Width, pictureBoxTake.Height);
-                    car.DrawAircraft(gr);
-                    pictureBoxTake.Image = bmp;
+                        Graphics gr = Graphics.FromImage(bmp);
+                        car.SetPosition(5, 15, pictureBoxTake.Width, pictureBoxTake.Height);
+                        car.DrawAircraft(gr);
+                        pictureBoxTake.Image = bmp;
+                    }
+                    else
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxTake.Width, pictureBoxTake.Height);
+                        pictureBoxTake.Image = bmp;
+                    }
+                    Draw();
                 }
-                else
-                {
-                    Bitmap bmp = new Bitmap(pictureBoxTake.Width, pictureBoxTake.Height);
-                    pictureBoxTake.Image = bmp;
-                }
-                Draw();
             }
+        }
+
+        private void listBoxLevels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Draw();
         }
     }
 }
